bd0b9c4ab8af2ce433c044d03c47a2800c721ce7
zbshadow <zbshadowx@gmail.com>
2024-11-23 15:33:03 -0500

    Mublazor added to project

--- FS25Calc.Web/FS25Calc.Web/Program.cs
+++ FS25Calc.Web/FS25Calc.Web/Program.cs
@@ -1,10 +1,12 @@
 using FS25Calc.Web.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
